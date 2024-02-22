a8478fc79f3f7ce16f5e8ed9a4125f6f6fa998a4
danrdo <danielle.shimomura@encora.com>
2024-02-22 16:21:05 -0300

    Criação de Issue via API

---
+++ cypress/e2e/api/createIssue.cy.js
@@ -0,0 +1,26 @@
+import { faker } from '@faker-js/faker'
+const accessToken = `Bearer ${Cypress.env('gitlab_access_token')}`
+
+describe('Create Issue', () => {
+   
+    beforeEach(() => cy.api_deleteProjects()) 
+  it('successfully', () => {
+
+    const issue = {
+        title: `issue-API-${faker.datatype.uuid()}`,
+        description: faker.random.words(3),
+        project: {
+          name: `project-API-${faker.datatype.uuid()}`,
+          description: faker.random.words(5)
+        }
+      }
+
+    cy.api_createIssue(issue)
+      .then(response => {
+        expect(response.status).to.equal(201)
+        expect(response.body.title).to.equal(issue.title)
+        expect(response.body.description).to.equal(issue.description)
+        console.log(response.body)
+      })
+   })
+})
