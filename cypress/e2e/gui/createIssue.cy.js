1cf2073b31154f9b69d2027b11efb35c844acdf1
danrdo <danielle.shimomura@encora.com>
2024-02-22 15:34:53 -0300

    alterando a criação de projeto para ser realizado via api

--- cypress/e2e/gui/createIssue.cy.js
+++ cypress/e2e/gui/createIssue.cy.js
@@ -15,7 +15,7 @@ describe('Create Issue', () => {
   beforeEach(() => {
     cy.login()
     cy.api_deleteProjects()
-    cy.gui_createProject(issue.project)
+    cy.api_createProject(issue.project)
   })
 
   it('successfully', () => {
