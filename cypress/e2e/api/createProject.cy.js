ac273416d5143f5f73d765995e56ed7235ba5d0f
danrdo <danielle.shimomura@encora.com>
2024-02-22 10:53:29 -0300

    Criação de projeto via API - correção

--- cypress/e2e/api/createProject.cy.js
+++ cypress/e2e/api/createProject.cy.js
@@ -1,24 +1,17 @@
 import { faker } from '@faker-js/faker'
-const token = `Bearer ${Cypress.env('gitlab_access_token')}`
 
-describe('Create Project - API', () => {
-    it('successfully', () => {
-        const project = {
-            name: `project-API-${faker.datatype.uuid()}`,
-            description: faker.random.words(5)
-        }
-        cy.request({
-            method: 'POST',
-            url: '/api/v4/projects/',
-            body: {name: project.name, 
-                   description: project.description, 
-                   initialize_with_readme: true},
-            headers:{Autorization: token}
+describe('Create Project', () => {
+  it('successfully', () => {
+    const project = {
+      name: `project-${faker.datatype.uuid()}`,
+      description: faker.random.words(5)
+    }
 
-        }).then(response =>{
-            expect(response.status).to.equal(201)
-            expect(response.body.name).to.equal(project.name)
-            expect(response.body.description).to.equal(project.description)
-        })
-    })
+    cy.api_createProject(project)
+      .then(response => {
+        expect(response.status).to.equal(201)
+        expect(response.body.name).to.equal(project.name)
+        expect(response.body.description).to.equal(project.description)
+      })
+  })
 })
